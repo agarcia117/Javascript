c3811f1b120c06857c9cf14c1772e769f96f81dd
Alan Garcia <43732771+agarcia117@users.noreply.github.com>
2024-02-21 11:02:26 -0800

    Completed Lesson 10

---
+++ scripts/10-rock-paper-scissors.js
@@ -0,0 +1,77 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
+
+updateScoreElement();
+
+function pickComputerMove() {
+const randomNumber = Math.random();
+let computerMove = '';
+
+if (randomNumber >= 0 && randomNumber < 1/3) {
+  computerMove = 'rock';
+} else if (randomNumber>= 1/3 && randomNumber < 2/3){
+  computerMove = 'paper';
+} else if (randomNumber >= 2/3 && randomNumber < 1){
+  computerMove = 'scissors';
+}
+
+return computerMove;
+}
+
+function playgame(playerMove, computerMove) {
+let result = '';
+
+if (computerMove === playerMove){
+  result = 'Tie.';
+} else if(playerMove === 'rock'){
+  if (computerMove === 'paper'){
+  result = 'You lose.';
+  }else if (computerMove === 'scissors'){
+    result = 'You win.';
+  }
+} else if (playerMove === 'paper'){
+  if (computerMove === 'scissors'){
+  result = 'You lose.';
+  }else if (computerMove === 'rock'){
+    result = 'You win.';
+  }
+} else if (playerMove === 'scissors'){
+  if (computerMove === 'rock'){
+  result = 'You lose.';
+  }else if (computerMove === 'paper'){
+    result = 'You win.';
+  }
+}
+
+if (result === 'You win.') {
+  score.wins += 1;
+} else if (result === 'You lose.') {
+  score.losses += 1;
+} else {
+  score.ties += 1;
+}
+
+localStorage.setItem('score', JSON.stringify(score));
+
+updateScoreElement();
+
+document.querySelector('.js-result')
+  .innerHTML = result;
+
+document.querySelector('.js-moves')
+.innerHTML = `You 
+<img src="images/${playerMove}-emoji.png" class="move-icon" alt="">
+<img src="images/${computerMove}-emoji.png" class="move-icon" alt="">
+Computer`;
+
+//         alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}
+// Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`);
+}
+
+function updateScoreElement() {
+document.querySelector('.js-score')
+.innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
