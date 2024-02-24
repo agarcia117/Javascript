cc0df2b0972c9bcc081b1034f5df8941f7115b69
Alan Garcia <43732771+agarcia117@users.noreply.github.com>
2024-02-24 11:56:19 -0800

    Finished Lesson 12 Part 1

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -5,8 +5,7 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = '';
   
-  for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
+  todoList.forEach(function (todoObject, index){
     const {name, dueDate} = todoObject;
     const html = `
       <div>
@@ -16,12 +15,12 @@ function renderTodoList() {
         ${dueDate}
       </div>
       <button onclick="
-        todoList.splice(${i}, 1);
+        todoList.splice(${index}, 1);
         renderTodoList();
       " class="delete-todo-button">Delete</button>
       `;
     todoListHTML += html;
-  }
+  });
     
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
